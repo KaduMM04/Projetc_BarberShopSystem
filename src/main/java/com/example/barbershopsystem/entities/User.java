487c621ab8bdc585b799359586623dbfd5082ed1
Kadu Miranda <kadumendesmiranda@gmail.com>
2025-03-07 18:11:08 -0300

    UserRepository create

--- src/main/java/com/example/barbershopsystem/entities/User.java
+++ src/main/java/com/example/barbershopsystem/entities/User.java
@@ -1,9 +1,6 @@
 package com.example.barbershopsystem.entities;
 
 import jakarta.persistence.MappedSuperclass;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.Objects;
 
---
+++ src/main/java/com/example/barbershopsystem/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.barbershopsystem.repositories;
+
+import  com.example.barbershopsystem.entities.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
