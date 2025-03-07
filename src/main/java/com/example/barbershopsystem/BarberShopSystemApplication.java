2807c8ded047ee0404c43ce0d71743bb489081c8
Kadu Miranda <kadumendesmiranda@gmail.com>
2025-03-07 17:43:48 -0300

    Project Created

---
+++ src/main/java/com/example/barbershopsystem/BarberShopSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.barbershopsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberShopSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BarberShopSystemApplication.class, args);
+    }
+
+}
